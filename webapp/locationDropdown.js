4fcc3a8a3e73308f96da4f56a2b57840e54d7a39
rdeiser <rdeiser@ksu.edu>
2024-09-27 09:30:50 -0500

    Add Vetmed to location validation

--- webapp/locationDropdown.js
+++ webapp/locationDropdown.js
@@ -1,26 +1,27 @@
 // START -- Enable user dropdown location validation -- added by K-State Libraires 06/2023
 
-    $(document).ready(function(){
-      var libraryObject = {
-        "Weigel": ["arch", "archoverrs", "archmapc", "archmedia", "archnew", "archover", "archper", "archref", "archspec"],
-        "Hale": ["cmc", "currper", "dowdis", "dowref", "gov", "govks", "govmap", "govmfile", "govmindex", "govoffmap", "halers", "juv", "main", "mediacoll", "medialp", "mediaover", "mic", "micref", "microfilm", "musart", "musartover", "musartref", "muscd", "muscdover", "musscore", "over", "overplus", "preslab", "ref", "sortmain", "sunderland", "tech", "techjstor", "techserial"]
-      }
-    
-      window.onload = function () {
-        var librarySel = document.getElementById("libraryCode");
-        var locationSel = document.getElementById("locationCode");
-    
-        for (var x in libraryObject) {
-          librarySel.options[librarySel.options.length] = new Option(x, x);
-        }
-        librarySel.onchange = function() {
-          var selectedLibrary = libraryObject[this.value];
-          locationSel.length = 1;
-          selectedLibrary.forEach(function(location) {
-            locationSel.options[locationSel.options.length] = new Option(location, location);
-          })
-        }
-      }
-    });
+$(document).ready(function(){
+  var libraryObject = {
+    "Hale": ["cmc", "currper", "dowdis", "dowref", "gov", "govks", "govmap", "govmfile", "govmindex", "govoffmap", "halers", "juv", "main", "mediacoll", "medialp", "mediaover", "mic", "micref", "microfilm", "musart", "musartover", "musartref", "muscd", "muscdover", "musscore", "over", "overplus", "preslab", "ref", "sortmain", "sunderland", "tech", "techjstor", "techserial"],
+    "Vetmed": ["vetm", "vetmrs", "vetmhist", "vetmjuv", "vetmnew", "vetmover", "vetmper", "vetmref", "vetmspec"],
+    "Weigel": ["arch", "archoverrs", "archmapc", "archmedia", "archnew", "archover", "archper", "archref", "archspec"]
+  }
+
+  window.onload = function () {
+    var librarySel = document.getElementById("libraryCode");
+    var locationSel = document.getElementById("locationCode");
+
+    for (var x in libraryObject) {
+      librarySel.options[librarySel.options.length] = new Option(x, x);
+    }
+    librarySel.onchange = function() {
+      var selectedLibrary = libraryObject[this.value];
+      locationSel.length = 1;
+      selectedLibrary.forEach(function(location) {
+        locationSel.options[locationSel.options.length] = new Option(location, location);
+      })
+    }
+  }
+});
 
 // END -- Enable user dropdown location validation -- added by K-State Libraires 06/2023
